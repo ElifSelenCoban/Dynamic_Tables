330e42d624d03562c13886ba430f4585cd6d8c1e
selencoban <elfslncbn@gmail.com>
2019-07-06 14:59:50 +0300

    Added files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,212 @@
+﻿using DynamicTables.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace DynamicTables.Controllers
+{
+    public class HomeController : Controller
+    {
+        Tables tableList = new Tables();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(tableList.FillList());
+        }
+         public JsonResult getCity(string id)
+        {
+            string countrystring = "select * from City where CountryId=" + id + "";
+            List<SelectListItem> list = new List<SelectListItem>();
+            tableList.Connection();
+            if (tableList.sqlConnection.State == ConnectionState.Open)
+            {
+                SqlCommand komut = new SqlCommand(countrystring, tableList.sqlConnection);
+                DataTable Tablo = new DataTable();
+                SqlDataAdapter Adaptor = new SqlDataAdapter(komut);
+                Adaptor.Fill(Tablo);
+
+                foreach (DataRow row in Tablo.Rows)
+                {
+
+                    list.Add(new SelectListItem { Text = Convert.ToString(row.ItemArray[1]), Value = Convert.ToString(row.ItemArray[0]) });
+                    
+                }
+                
+            }
+            return Json(new { result = list }, JsonRequestBehavior.AllowGet);
+        }
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            FillforeignDictionay();
+            ViewBag.prikeylist = Tables.prilist;
+            return View(tableList.GetTableColumn());
+        }
+        public SelectList GenelSelectList(string ad)
+
+        {
+            List<SelectListItem> genel = new List<SelectListItem>();
+            int a = tableList.GetTableColumn().Rows.Count;
+            string b;
+            b = "select  * " + " from " + ad;
+
+            tableList.Connection();
+            if (tableList.sqlConnection.State == ConnectionState.Open)
+            {
+                SqlCommand komut = new SqlCommand(b, tableList.sqlConnection);
+                DataTable Tablo = new DataTable();
+                SqlDataAdapter Adaptor = new SqlDataAdapter(komut);
+                Adaptor.Fill(Tablo);
+
+                foreach (DataRow row in Tablo.Rows)
+                {
+
+                    genel.Add(new SelectListItem { Text = Convert.ToString(row.ItemArray[1]), Value = Convert.ToString(row.ItemArray[0]) });
+                    
+                }
+            }
+            return new SelectList(genel, "Value", "Text" );
+        }
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            try
+            {
+                tableList.Connection();
+                string sqlcom = "";
+                sqlcom = "insert into " + Tableproperty.tablename + " (";
+                int index1;
+                foreach (var key in form.AllKeys)
+                {
+                    if (!Tables.prilist.Contains(key))
+                    { sqlcom += key + " ,"; }
+
+
+                }
+                index1 = sqlcom.LastIndexOf(',');
+                sqlcom = sqlcom.Remove(index1, 1);
+                sqlcom += ") values ('";
+                foreach (var key in form.AllKeys)
+                {
+                    if (!Tables.prilist.Contains(key))
+                    { sqlcom += form[key] + "','"; }
+
+
+                }
+                index1 = sqlcom.LastIndexOf(','); 
+                sqlcom = sqlcom.Remove(index1, 1);
+                index1 = sqlcom.LastIndexOf('\'');
+                sqlcom = sqlcom.Remove(index1, 1);
+                sqlcom += ")";
+
+                SqlCommand komut = new SqlCommand(sqlcom, tableList.sqlConnection);
+
+                komut.ExecuteNonQuery();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                FillforeignDictionay();//foreignkey listesini doldurup viewa yönlendirir
+                ViewBag.prikeylist = Tables.prilist;//primarykey listesini doldurup viewa yönlendirir
+                ViewBag.Hata = "Islem Gerceklesmedi! \n Lütfen bütün alanları doldurarak tekrar deneyiniz.";//Hata olusursa mesajı viewa yönlendirir
+                return View(tableList.GetTableColumn());//kolon isimlerini gönderir
+            }
+
+        }
+        // GET: Home/Edit/5
+        public ActionResult Edit(String Table_name)
+        {
+            return View(tableList.FillByTableName(Table_name));//secilen tabloya göre verileri listelenmesi için FillByTableName methodu çağrılır
+        }
+        public ActionResult EditRecord(int id)
+        {
+            FillforeignDictionay();//foreignkeyi doldurarak viewa gönderilir
+            ViewBag.prikeylist = Tables.prilist;//primarykey listesi doldurulur ve viewa göndeerilir
+             return View(tableList.Recordİnfobyid(id, Tableproperty.tablename));//verilen id ye göre veriler doldurulur recordinfobyid methodu ile
+
+        }
+        [HttpPost]
+        public ActionResult EditRecord(int id, FormCollection form)//verilen idye ait bilgilerin güncellenmesi
+        {
+            try
+            {
+                tableList.Connection();
+
+                string sqlcom = "";
+                sqlcom = "Update  " + Tableproperty.tablename + "  set ";
+
+
+                foreach (var key in form.AllKeys)
+                {
+                    if (!Tables.prilist.Contains(key))//primarykey değilse kolon ismi ve değeri formcollectiondaki key kullanılarak sorguya eklenir
+                    {
+                        sqlcom += key + " = '" + form[key] + "' ,";
+                    }
+
+                }
+                int index1 = sqlcom.LastIndexOf(',');//sorgu sonundaki , karakterini ulur ve kaldırır
+                sqlcom = sqlcom.Remove(index1, 1);
+                sqlcom += " where " + Tables.prilist[0].ToString() + "=" + id;//primarykey ismi listeden çekilip parametre gelen idye eşlenir 
+                SqlCommand komut = new SqlCommand(sqlcom, tableList.Connection());
+                komut.ExecuteNonQuery();
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                FillforeignDictionay();//foreignkey dolduruluo viewbag kullanılarak viewa iletilir
+                ViewBag.prikeylist = Tables.prilist;//primarykey listesi doldurulup viewa gönderilir
+                ViewBag.Hata = "Islem Gerceklesmedi! \n Lütfen bütün alanları doldurarak tekrar deneyiniz.";//Edit işlemi gerçekleşmezse hata mesajını viewa gönderir
+                return View(tableList.Recordİnfobyid(id, Tableproperty.tablename));//verilen id ye göre veriler doldurulur recordinfobyid methodu ile
+
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                tableList.delete(id);// verilen id ye göre delete metodu çalıştırılır
+
+            
+            }
+            catch
+            {
+                TempData["Hata"] = "Silme Islemi Gercekleşmedi. Tekrar Deneyiniz! ";//Silme işlemi gerçekleşmediğinde index sayfasına hata gönderir.
+            }
+            return RedirectToAction("Index");
+        }
+
+        public void FillforeignDictionay()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            DataTable dataTable = tableList.GetTableColumn();
+            int a = tableList.GetTableColumn().Rows.Count;
+
+            if (dataTable.Rows.Count != 0)
+            {
+                for (int i = 0; i < a; i++)
+                {
+
+                    if (dataTable.Rows[i]["Referenced_column_name"].ToString() != "")//foreignkeyi varsa 
+                    {
+                        dictionary.Add(dataTable.Rows[i]["Referenced_column_name"].ToString(), dataTable.Rows[i]["Referenced_object"].ToString());
+                        //foreignkey kolonunu ve referenced tablonun ismi dictionarye eklenir
+
+                    }
+                }
+
+                ViewBag.foreigncolumn = dictionary;//dictionary viewa gönderilir
+            }
+        }
+    }
+}
---
+++ Models/Tables.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicTables.Models
+{
+    public class Tables
+    {
+
+        public SqlConnection sqlConnection;
+        SqlDataAdapter Adaptor;
+        DataTable Tablo;
+        SqlCommand komut;
+        public static List<string> prilist = new List<string>();
+        public List<Tableproperty> tablenameList { get; set; }
+        public SqlConnection Connection()
+        {
+            sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = "Data Source=DESKTOP;Initial Catalog=DOS;Integrated Security=True;";
+            sqlConnection.Open();
+            return sqlConnection;
+        }
+        public List<Tableproperty> FillList()
+        {
+            Connection();
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                komut = new SqlCommand("GetTableNames", sqlConnection);
+                komut.CommandType = CommandType.StoredProcedure;
+                Tablo = new DataTable();
+                Adaptor = new SqlDataAdapter(komut);
+                Adaptor.Fill(Tablo);
+                tablenameList = new List<Tableproperty>();
+                for (int i = 0; i < Convert.ToInt32(Tablo.Rows.Count); i++)
+                {
+                    Tableproperty tableproperty = new Tableproperty();
+                    tableproperty.TABLE_NAME = Convert.ToString(Tablo.Rows[i]["TABLE_NAME"]);
+                    tablenameList.Add(tableproperty);
+                }
+
+            }
+            return tablenameList;
+        }
+        public DataTable FillByTableName(string tablenames) 
+        {
+           
+            Tableproperty.tablename = tablenames;
+            prikey();
+            Connection();
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                komut = new SqlCommand("GetRecordofDynamictable", sqlConnection);
+                komut.CommandType = CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@tableName", Tableproperty.tablename);
+                Tablo = new DataTable();
+                Adaptor = new SqlDataAdapter(komut);
+                Adaptor.Fill(Tablo);
+            }
+            return Tablo;
+        }
+        public DataTable Recordİnfobyid(int i, string tablename)
+        {
+          
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                komut = new SqlCommand("Recordİnfobyid", sqlConnection);
+                komut.CommandType = CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@id", i);
+                komut.Parameters.AddWithValue("@tableName", Tableproperty.tablename);
+                komut.Parameters.AddWithValue("@pri", prilist[0]);
+                Tablo = new DataTable();
+                Adaptor = new SqlDataAdapter(komut);
+                Adaptor.Fill(Tablo);
+            }
+            return Tablo;
+        }
+        public List<string> prikey()
+        {
+
+            Connection();
+            prilist.Clear();
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                komut = new SqlCommand("prikey", sqlConnection);
+                komut.CommandType = CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@TableName", Tableproperty.tablename);
+                Tablo = new DataTable();
+                Adaptor = new SqlDataAdapter(komut);
+                Adaptor.Fill(Tablo);
+                int satir = Convert.ToInt32(Tablo.Rows.Count);
+
+                for (int i = 0; i < satir; i++)
+                {
+                    string a;
+                    a = Tablo.Rows[i]["COLUMN_NAME"].ToString(); 
+                    prilist.Add(a);
+                }
+
+
+            }
+
+            return prilist;
+        }
+        public void delete(int id)
+        {
+                Connection();
+                if (sqlConnection.State == ConnectionState.Open)
+                {
+                    string x = "Delete from " + Tableproperty.tablename + " where " + prilist[0].ToString() + " =" + id; // tablo ismi ve gelen id nin eşitleneceği primarykey verilen delete sorgusu
+
+                    komut = new SqlCommand(x, sqlConnection);
+
+                    komut.ExecuteNonQuery();
+
+                }
+                Connection().Close();
+            
+
+        }
+        public DataTable GetTableColumn()
+        {
+            DataTable createtable = new DataTable();
+            Connection();
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                komut = new SqlCommand("getcolumnnamewithforeigtable", sqlConnection);
+                komut.CommandType = CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@Tables", Tableproperty.tablename);
+                Adaptor = new SqlDataAdapter(komut);
+                Adaptor.Fill(createtable);
+            }
+            return createtable;
+        }
+    }
+    public class Tableproperty
+    {
+        public String TABLE_NAME { get; set; }
+        public static String tablename { get; set; }
+    } 
+
+}
+
